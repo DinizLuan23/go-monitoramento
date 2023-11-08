0e27270878d7cec90d31d417e3f45782eef1763b
Luan Diniz <luan.diniz23@hotmail.com>
2023-11-08 13:57:56 -0300

    feat: salvar logs e exibir

--- hello.go
+++ hello.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
-	"bufio"
+	"strconv"
 )
 
-const monitoramentos = 3
+const monitoramentos = 1
 const delay = 10
 
 func main() {
 	exibeIntroducao()
-	leSitesDoArquivo()
 
 	for {
 		exibeMenu()
@@ -24,6 +26,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -60,12 +63,6 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	// sites := []string{
-	// 	"https://random-status-code.herokuapp.com/",
-	// 	"https://alura.com.br/",
-	// 	"https://poupacred.com.br/",
-	// 	"https://google.com/",
-	// }
 
 	sites := leSitesDoArquivo()
 
@@ -91,8 +88,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -107,13 +106,39 @@ func leSitesDoArquivo() []string {
 
 	leitor := bufio.NewReader(arquivo)
 
-	linha, erro := leitor.ReadString('\n')
-
-	if erro != nil {
-		fmt.Println("Ocorreu um erro", err)
+	for {
+		linha, erro := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		
+		sites = append(sites, linha)
+		
+		if erro == io.EOF {
+			break
+		}
 	}
-
-	fmt.Println(linha)
+	
+	arquivo.Close()
 
 	return sites
+}
+
+func registraLog(site string, status bool){
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
+}
+
+func imprimeLogs(){
+	arquivo, err := os.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
